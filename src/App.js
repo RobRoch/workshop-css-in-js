2ccf68e91a8e0c7aa29d4414d629853437ba2db6
RobRoch <rochon18@gmail.com>
2019-08-03 10:15:39 +0200

    task1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.scss';
 
+/* TODO - style box in scss */
+
 function App() {
-  return <div className='app' />;
+  return (
+    <div className='app'>
+      <div className='box' />
+    </div>
+  );
 }
 
 export default App;
